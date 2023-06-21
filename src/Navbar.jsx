58ec313b89af82cce13def3d3869e9106e63ed3e
Kyle Stevens <kyleastevens3@gmail.com>
2023-06-21 12:38:24 -0700

    navbar complete w routing

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,22 +1,32 @@
 import React from 'react';
+import { Link, useMatch, useResolvedPath } from 'react-router-dom';
 
 function Navbar() {
   return (
     <nav className="nav">
-      <a href="/" className="site-title">Kyles Page</a>
+      <Link to="/" className="site-title">
+        Kyles Page
+      </Link>
       <ul>
-        <li>
-          <a href="/about">About</a>
-        </li>
-        <li>
-          <a href="/projects">Projects</a>
-        </li>
-        <li>
-          <a href="/contact">Contact</a>
-        </li>
+        <CustomLink to="/about">About</CustomLink>
+        <CustomLink to="/projects">Projects</CustomLink>
+        <CustomLink to="/contact">Contact</CustomLink>
       </ul>
     </nav>
   );
+}
+
+function CustomLink({ to, children, ...props }) {
+  const resolvedPath = useResolvedPath(to);
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true })
+
+  return (
+    <li className={isActive ? "active" : ""}>
+      <Link to={to} {...props}>
+        {children}
+      </Link>
+    </li>
+  )
 }
 
 export default Navbar;
